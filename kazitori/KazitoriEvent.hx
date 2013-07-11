2d7b35fe7a0d1fb95f13e41dd67f723230ad6fd6
Yamaguchi Eikichi <megane@glasses-factory.net>
2013-07-11 18:35:57 +0900

    Event ちゃんと動くように

--- kazitori/KazitoriEvent.hx
+++ kazitori/KazitoriEvent.hx
@@ -1,29 +1,30 @@
 package kazitori;
 
+@:native('KazitoriEvent')
 extern class KazitoriEvent {
     public function new(type:String, next:String, prev:String):Void;
     public function clone():KazitoriEvent;
     public function toString():String;
 
 
-    // 
-    public static var TASK_QUEUE_COMPLETE(default, "task_queue_complete"):String;
-    public static var TASK_QUEUE_FAILED(default, "task_queue_failed"):String;
-    public static var CHANGE(default, "change"):String;
-    public static var EXECUTED(default, "executed"):String;
-    public static var BEFORE_EXECUTED(default, "before_executed"):String;
-    public static var INTERNAL_CHANGE(default, "internal_change"):String;
-    public static var USER_CHANGE(default, "user_change"):String;
-    public static var PREV(default, "prev"):String;
-    public static var NEXT(default, "next"):String;
-    public static var REJECT(default, "reject"):String;
-    public static var NOT_FOUND(default, "not_found"):String;
-    public static var START(default, "start"):String;
-    public static var STOP(default, "stop"):String;
-    public static var SUSPEND(default, "suspend"):String;
-    public static var RESUME(default, "resume"):String;
-    public static var FIRST_REQUEST(default, "first_request"):String;
-    public static var ADDED(default, "added"):String;
-    public static var REMOVED(default, "removed"):String;
+    //
+    public static var TASK_QUEUE_COMPLETE:String;
+    public static var TASK_QUEUE_FAILED:String;
+    public static var CHANGE:String;
+    public static var EXECUTED:String;
+    public static var BEFORE_EXECUTED:String;
+    public static var INTERNAL_CHANGE:String;
+    public static var USER_CHANGE:String;
+    public static var PREV:String;
+    public static var NEXT:String;
+    public static var REJECT:String;
+    public static var NOT_FOUND:String;
+    public static var START:String;
+    public static var STOP:String;
+    public static var SUSPEND:String;
+    public static var RESUME:String;
+    public static var FIRST_REQUEST:String;
+    public static var ADDED:String;
+    public static var REMOVED:String;
 
 }
